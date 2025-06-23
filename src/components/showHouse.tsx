dfb9d78a2035fee627ba95df723efaf9abe5ca8d
Andrew <mcdude22@gmail.com>
2025-06-22 17:59:14 -0700

    Numbers are pretty now

--- src/components/showHouse.tsx
+++ src/components/showHouse.tsx
@@ -12,14 +12,17 @@ const toggle = (id: number) => {
 }
 
 const ShowHouse: Component<HouseProps> = (props) => {
-
+	let totalBudget = 0;
+	props.house.rooms.forEach((room) => totalBudget += room.budget)
 	return (
 		<div>
+			<p>Total house budget: ${totalBudget.toLocaleString()}</p>
 			<For each={props.house.rooms}>
 				{(room) => <div>
-					<p><input type="checkbox" checked={room.completed} onChange={() => toggle(room.id)} />{room.name}: {room.xDim}x{room.yDim}, ${room.budget}</p>
+					<p><input type="checkbox" checked={room.completed} onChange={() => toggle(room.id)} />{room.name}: {room.xDim}x{room.yDim}, ${room.budget.toLocaleString()}</p>
 				</div>}
 			</For>
+
 
 		</div>
 	);
